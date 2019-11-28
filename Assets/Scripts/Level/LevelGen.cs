53df5dbd00b686b4fe1686993e8f450bf270f53b
killersshadow <32833605+killersshadow@users.noreply.github.com>
2019-11-28 12:40:19 +0100

    start project

---
+++ Assets/Scripts/Level/LevelGen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGen : MonoBehaviour
+{
+    [SerializeField]private GameObject floor;
+    private GameObject[] floors;
+    public int gridSizeX, gridSizeZ;
+
+    void Start()
+    {
+        for (int i = 0; i < gridSizeX; i++)
+        {
+            Instantiate(floor, new Vector3(i * 5,0,0), Quaternion.identity);
+
+            for (int j = 0; j < gridSizeZ; j++)
+            {
+                Instantiate(floor, new Vector3(j * 5, 0, i * -5), Quaternion.identity);
+                if (j == gridSizeX)
+                {
+                    j = 0;
+                }
+            }
+        }
+    }//INFINIT LOOP WARNING!
+
+    void Update()
+    {
+        
+    }
+}
